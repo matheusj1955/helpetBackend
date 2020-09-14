5b4965113569e5bbdfd13a90df0292eeb728a492
Matheus J <matheus.pro1995@gmail.com>
2020-09-14 20:56:46 -0300

    apos api funcionar, ajustando tudo denovo

--- controllers/usuarios-controller.js
+++ controllers/usuarios-controller.js
@@ -1,11 +1,12 @@
-const mysql = require('../mysql');
+const mysql = require('../mysql').pool;
 
 exports.getUsuarios = (req, res, next) => {
 	mysql.getConnection((error,conn) => {
 		if(error){ return res.status(500).send({ error: error }) }
 		conn.query(
-		'SELECT * FROM usuarios;',
+		'SELECT * FROM helpet.usuarios;',
 			(error, result, fields) => {
+			    conn.release();
 				if(error){ return res.status(500).send({ error: error }) }
 				const response = {
 					usuarios: result.map(usu => {
@@ -17,75 +18,75 @@ exports.getUsuarios = (req, res, next) => {
 									tipo: 'GET',
 									descricao: 'retorna todos os usuarios',
 									url: 'http://localhost:3000/usuarios'
-                                           }
-                                          }
-                                        })
-                                    }
-                                return res.status(201).send(response);
+                                }
                             }
-                        )
-                           });
+                        })
                     }
+                return res.status(201).send(response);
+            }
+        )
+    });
+}
 
 exports.postCriaUsuario = async (req, res, next) => {
-//    try {
-//        const query = 'INSERT INTO usuarios (nome, tel, email) VALUES (?,?,?)';
-//        const result = await mysql.execute(query, [
-//            req.body.nome,
-//            req.body.tel,
-//            req.body.email,
-////            req.file.path
-//        ]);
-//
-//        const response = {
-//            message: 'usuario criado com sucesso',
-//            criandoUsuario: {
-//                id_usuario: result.insertId,
-//                req.body.nome,
-//                req.body.tel,
-//                req.body.email,
-////                imagemUsuario: req.file.path,
-//                request: {
-//                    type: 'GET',
-//                    description: 'Retorna todos os usuarios',
-//                    url: process.env.URL_API + 'usuarios'
-//                }
-//            }
-//        }
-//        return res.status(201).send(response);
-//    } catch (error) {
-//        return res.status(500).send({ error: error });
-//    }
+	mysql.getConnection((error,conn) => {
+		if(error){ return res.status(500).send({ error: error }) }
+		conn.query(
+		'INSERT INTO usuarios (nome, tel, email) VALUES (?,?,?)',
+		[req.body.nome, req.body.tel, req.body.email],
+            (error, result, field) => {
+			    conn.release();
+				if(error){ return res.status(500).send({ error: error }) }
+				const response = {
+					mensagem: 'Usuario inserido com sucesso',
+					criandoUsuario: {
+							id_usuario: result.id_usuario,
+							nome: req.body.nome,
+							email: req.body.email,
+								request: {
+									tipo: 'GET',
+									descricao: 'retorna todos os usuarios',
+									url: 'http://localhost:3000/usuarios'
+                                }
+                            }
+                    }
+                return res.status(201).send(response);
+            }
+        )
+    });
 };
 
 exports.getUmUsuario = async (req, res, next)=> {
-//    try {
-//        const query = 'SELECT * FROM usuarios WHERE id_usuario = ?;';
-//        const result = await mysql.execute(query, [req.params.id_usuario]);
-//
-//        if (result.length == 0) {
-//            return res.status(404).send({
-//                message: 'Não foi encontrado usuario com este ID'
-//            })
-//        }
-//        const response = {
-//            product: {
-//                id_usuario: result[0].id_usuario,
-//                nome: result[0].nome,
-//                tel: result[0].tel,
-//                email: result[0].email,
-//                request: {
-//                    type: 'GET',
-//                    description: 'Retorna todos os usuarios',
-//                    url: process.env.URL_API + 'usuarios'
-//                }
-//            }
-//        }
-//        return res.status(200).send(response);
-//    } catch (error) {
-//        return res.status(500).send({ error: error });
-//    }
-};
+ 	mysql.getConnection((error,conn) => {
+		if(error){ return res.status(500).send({ error: error }) }
+		conn.query(
+			'SELECT * FROM usuarios WHERE id_usuario = ?;',
+			[req.params.id_usuario],
+			(error, result, fields) => {
+				if(error){ return res.status(500).send({ error: error }) }
+				if(result.length == 0) {
+					return res.status(404).send({
+						mensagem: 'Não foi encontrado usuario com esse ID'
+                       				 })
+                  			  }
+					const response = {
+					usuario:{
+						id_usuario: result[0].id_usuario,
+						nome: result[0].nome,
+						email: result[0].email,
+							request: {
+								tipo: 'GET',
+								descricao: 'retorna todos os usuarios',
+								url: 'http://localhost:3000/usuarios'
+                             	}
+                            }
+                        }
+                return res.status(201).send(response);
+            }
+        )
+    });
+}
+
 
 exports.patchAtualizaUsuario = async (req, res, next) => {
 //
